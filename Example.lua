bc8920e699a41781b9fec07515d3ec83b24d0025
Monia <102583640+NTDCore@users.noreply.github.com>
2025-08-03 18:42:01 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,8 +1,8 @@
 -- example script by https://github.com/mstudio45/LinoriaLib/blob/main/Example.lua and modified by deivid
 -- You can suggest changes with a pull request or something
 
-local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
-local Library = loadstring(readfile('GitHub/Obsidian/Library.lua'), 'Library')() --loadstring(game:HttpGet(repo .. "Library.lua"))()
+local repo = "https://raw.githubusercontent.com/NTDCore/Obsidian/main/"
+local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
 
